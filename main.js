e7e76907f7966f4779ece231494f9029864359a2
Russell <russelldev@protonmail.com>
2020-11-20 21:10:17 +0000

    Add event listener on click

--- main.js
+++ main.js
@@ -5,3 +5,7 @@ const selectElement = (element) => {
 
 const menuToggler = selectElement('.menu-toggle');
 const body = selectElement('body');
+
+menuToggler.addEventListener('click', ()=>{
+    body.classList.toggle('open');
+});
