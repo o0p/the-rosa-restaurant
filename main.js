32e11113692e1ecd51908638f40445d9ed0aabc9
Russell <russelldev@protonmail.com>
2020-11-28 19:34:08 +0000

    Add main.js scroll reveal left

--- main.js
+++ main.js
@@ -9,3 +9,13 @@ const body = selectElement('body');
 menuToggler.addEventListener('click', ()=>{
     body.classList.toggle('open');
 });
+
+//Scroll reveal
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left',{
+    origin: 'left',
+    duration: 1000,
+    distance: '25rem',
+    delay: 50
+});
